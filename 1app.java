a3280c9c48006790ba7cf465016dfa6ae07f7538
shyamkumarraju032619-prog <shyamkumarraju032619@gmail.com>
2026-02-03 15:16:17 +0530

    Create 1app.java

---
+++ 1app.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Jenkins WAR build!</h1>");
+    }
+}
